813b944706884d301e3cfcf9ee300f53bc616143
Jeff Harrell <jeff@juxtadesign.com>
2013-12-18 07:55:26 -0800

    Adding 503 into the scaffolded templates

--- app/index.js
+++ app/index.js
@@ -151,6 +151,8 @@ Generator.prototype.projectfiles = function projectfiles() {
     this.template('public/templates/layouts/_master.dust', 'public/templates/layouts/master.dust');
     this.template('public/templates/errors/_404.dust', 'public/templates/errors/404.dust');
     this.template('public/templates/errors/_500.dust', 'public/templates/errors/500.dust');
+    this.template('public/templates/errors/_503.dust', 'public/templates/errors/503.dust');
     this.copy('content/404.properties', 'locales/US/en/errors/404.properties');
     this.copy('content/500.properties', 'locales/US/en/errors/500.properties');
+    this.copy('content/503.properties', 'locales/US/en/errors/503.properties');
 };
