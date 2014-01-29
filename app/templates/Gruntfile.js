e949cf6556108b1b92bc5dbadcfadf8ec61d460f
lmarkus <lmarkus@paypal.com>
2014-01-29 09:37:02 -0800

    Added more useful comments

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -53,7 +53,9 @@ module.exports = function (grunt) {
                     fullname: function (filepath) {
                         var path = require('path'),
                             name = path.basename(filepath, '.dust'),
-                        //Hardcoded to forwards slash on purpose. Keeps compatibility on win_32
+                        //Hardcoded to forwards slash on purpose. This is due to the way that grunt handles globbing.
+                        //Patterns like **/*.dust are always expanded using '/' with no consideration to the host OS
+                        //separator. This caused issues when trying to build on win_32
                             parts = filepath.split('/'),
                             fullname = parts.slice(3, -1).concat(name);
 
