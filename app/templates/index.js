9531a82cae24446bc12cd16e25d0a892417b1c05
Jeff Harrell <jeff@juxtadesign.com>
2014-03-12 10:30:50 -0700

    initial 1.0 interop

--- app/templates/index.js
+++ app/templates/index.js
@@ -1,38 +1,15 @@
 'use strict';
 
 
-var kraken = require('kraken-js'),
-    app = {};
+var kraken = require('kraken.next'),
+    express = require('express'),
+    app = express(),
+    port = process.env.PORT || 8000;
 
 
-app.configure = function configure(nconf, next) {
-    // Async method run on startup.
-    next(null);
-};
+app.use(kraken());
 
 
-app.requestStart = function requestStart(server) {
-    // Run before most express middleware has been registered.
-};
-
-
-app.requestBeforeRoute = function requestBeforeRoute(server) {
-    // Run before any routes have been added.
-};
-
-
-app.requestAfterRoute = function requestAfterRoute(server) {
-    // Run after all routes have been added.
-};
-
-
-if (require.main === module) {
-    kraken.create(app).listen(function (err, server) {
-        if (err) {
-            console.error(err.stack);
-        }
-    });
-}
-
-
-module.exports = app;
+app.listen(port, function () {
+    console.log('Listening on http://127.0.0.1:8000/');
+});
