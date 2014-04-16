5206eaaa75bc31a6f5f824bd252f5dc5c56903fe
poornima <pvenkatakrishnan@paypal.com>
2014-04-16 16:21:15 -0700

    Some bug fixes

--- app/templates/common/lib/spec.js
+++ app/templates/common/lib/spec.js
@@ -4,10 +4,8 @@
 module.exports = function spec(app) {
 
     return {
-        onconfig: function (config, next) {<% if (templateModule) { %>
-        <% if (i18n) { %>
+        onconfig: function (config, next) {<% if (templateModule && i18n) { %>
             config.get('view engines:js:renderer:arguments').push(engine, app);
-            <% } %>
             <% } %>
             next(null, config);
         }
