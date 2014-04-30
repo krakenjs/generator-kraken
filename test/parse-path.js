8f4dcf61e69d551a05634b06e12cfcc012057df2
Jean-Charles Sisk <jasisk@gmail.com>
2014-04-30 16:34:43 -0400

    adding parse-path util

---
+++ test/parse-path.js
@@ -0,0 +1,88 @@
+/*───────────────────────────────────────────────────────────────────────────*\
+│  Copyright (C) 2014 eBay Software Foundation                                │
+│                                                                             │
+│hh ,'""`.                                                                    │
+│  / _  _ \  Licensed under the Apache License, Version 2.0 (the "License");  │
+│  |(@)(@)|  you may not use this file except in compliance with the License. │
+│  )  __  (  You may obtain a copy of the License at                          │
+│ /,'))((`.\                                                                  │
+│(( ((  )) ))    http://www.apache.org/licenses/LICENSE-2.0                   │
+│ `\ `)(' /'                                                                  │
+│                                                                             │
+│   Unless required by applicable law or agreed to in writing, software       │
+│   distributed under the License is distributed on an "AS IS" BASIS,         │
+│   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  │
+│   See the License for the specific language governing permissions and       │
+│   limitations under the License.                                            │
+\*───────────────────────────────────────────────────────────────────────────*/
+/*global describe, it*/
+
+'use strict';
+
+var assert = require('assert');
+var parsePath = require('../util/parse-path');
+
+
+describe('parsePath', function () {
+
+  it('should parse "index" correctly', function () {
+    var expectation = {
+      base: 'index',
+      dir: '.',
+      root: '..',
+      route: '/',
+      model: 'index',
+      fullname: 'index'
+    };
+    assertPath('index', expectation);
+    assertPath('index.js', expectation);
+    assertPath('./index', expectation);
+    assertPath('./index.js', expectation);
+  });
+
+  it('should parse "new" correctly', function () {
+    var expectation = {
+      base: 'index',
+      dir: 'new',
+      root: '../..',
+      route: '/',
+      model: 'new',
+      fullname: 'new/index'
+    };
+    assertPath('new', expectation);
+    assertPath('new/', expectation);
+    assertPath('new/index', expectation);
+    assertPath('new/index.js', expectation);
+  });
+
+  it('should parse "new/sub" correctly', function () {
+    var expectation = {
+      base: 'index',
+      dir: 'new/sub',
+      root: '../../..',
+      route: '/',
+      model: 'sub',
+      fullname: 'new/sub/index'
+    };
+    assertPath('new/sub', expectation);
+    assertPath('new/sub/', expectation);
+    assertPath('new/sub/index', expectation);
+    assertPath('new/sub/index.js', expectation);
+  });
+});
+
+function assertPath(name, expectedParts) {
+  var parts = parsePath(name);
+  assert(compareObjects(parts, expectedParts), name + ' doesn\'t equal the expected parts');
+}
+
+function compareObjects(actual, expected) {
+  return Object.keys(expected).every(function (key) {
+    try {
+      assert.deepEqual(expected[key], actual[key]);
+    } catch (e) {
+      return false;
+    }
+    return true;
+  });
+}
---
+++ util/parse-path.js
@@ -0,0 +1,52 @@
+/*───────────────────────────────────────────────────────────────────────────*\
+│  Copyright (C) 2014 eBay Software Foundation                                │
+│                                                                             │
+│hh ,'""`.                                                                    │
+│  / _  _ \  Licensed under the Apache License, Version 2.0 (the "License");  │
+│  |(@)(@)|  you may not use this file except in compliance with the License. │
+│  )  __  (  You may obtain a copy of the License at                          │
+│ /,'))((`.\                                                                  │
+│(( ((  )) ))    http://www.apache.org/licenses/LICENSE-2.0                   │
+│ `\ `)(' /'                                                                  │
+│                                                                             │
+│   Unless required by applicable law or agreed to in writing, software       │
+│   distributed under the License is distributed on an "AS IS" BASIS,         │
+│   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  │
+│   See the License for the specific language governing permissions and       │
+│   limitations under the License.                                            │
+\*───────────────────────────────────────────────────────────────────────────*/
+'use strict';
+
+var path = require('path');
+
+function parsePath(name) {
+
+  var DEFAULT_IDX = 'index';
+
+  var parts = {
+    ext: path.extname(name),
+    dir: path.dirname(name),
+    model: 'index',
+    route: '/'
+  };
+
+  parts.base = path.basename(name, parts.ext) || DEFAULT_IDX;
+
+  if (parts.base.toLowerCase() !== DEFAULT_IDX) {
+    parts.model = parts.base;
+    parts.dir = path.join(parts.dir, parts.base);
+    parts.base = DEFAULT_IDX;
+  } else {
+    var newBase = path.basename(parts.dir);
+    if (newBase !== '.') {
+      parts.model = newBase;
+    }
+  }
+
+  parts.fullname = path.join(parts.dir, parts.base);
+  parts.root = path.relative(parts.fullname, './');
+
+  return parts;
+}
+
+module.exports = parsePath;
