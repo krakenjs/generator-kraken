13f69587e6358b78e56e8a719f1149d64b5a0040
poornima <pvenkatakrishnan@paypal.com>
2014-06-05 13:24:05 -0700

    Removing the lib/spec.js generation

--- app/templates/common/index.js
+++ app/templates/common/index.js
@@ -3,7 +3,12 @@
 
 var kraken = require('kraken-js'),
     app = require('express')(),
-    options = require('./lib/spec')(),
+    options = {
+        onconfig: function (config, next) {
+            //any config setup/overrides here
+            next(null, config);
+        }
+    },
     port = process.env.PORT || 8000;
 
 
