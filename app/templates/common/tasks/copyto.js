1c46b07eeee10d85e1503ad897e54a9961943359
Aria Stewart <aredridel@dinhe.net>
2014-12-08 13:35:49 -0500

    Replace tabs with spaces in tasks

--- app/templates/common/tasks/copyto.js
+++ app/templates/common/tasks/copyto.js
@@ -5,21 +5,21 @@ module.exports = function copyto(grunt) {
     // Load task
     grunt.loadNpmTasks('grunt-copy-to');
 
-	// Options
-	return {
-	    build: {
+    // Options
+    return {
+        build: {
             files: [{
                 cwd: 'public',
                 src: ['**/*'],
                 dest: '.build/'
             }],
-	        options: {
-	            ignore: [
-	            	<% if (cssModule) { %>'public/css/**/*',<% } %>
-	                <% if (jsModule) { %>'public/js/**/*',<% } %>
-	                'public/templates/**/*'
-	            ]
-	        }
-	    }
-	};
+            options: {
+                ignore: [
+                    <% if (cssModule) { %>'public/css/**/*',<% } %>
+                    <% if (jsModule) { %>'public/js/**/*',<% } %>
+                    'public/templates/**/*'
+                ]
+            }
+        }
+    };
 };
